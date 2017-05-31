14f3820216e7b302c2a1f6983bfb6e5630114298
Johnsonlee <johnsonlee28@gmail.com>
2017-06-01 00:01:11 +0800

    add time select

--- app/controllers/admin/event_registrations_controller.rb
+++ app/controllers/admin/event_registrations_controller.rb
@@ -5,6 +5,22 @@ class Admin::EventRegistrationsController < ApplicationController
   def index
     @registrations = @event.registrations.includes(:ticket).order("id DESC").page(params[:page]).per(10)
 
+    if params[:start_on].present?
+      @registrations = @registrations.where( "created_at >= ?", Date.parse(params[:start_on]).beginning_of_day)
+    end
+
+    if params[:end_on].present?
+      @registrations = @registrations.where( "created_at <= ?", Date.parse(params[:end_on]).end_of_day )
+    end
+
+    if Array(params[:statuses]).any?
+      @registrations = @registrations.by_status(params[:statuses])
+    end
+
+    if Array(params[:ticket_ids]).any?
+      @registrations = @registrations.by_ticket(params[:ticket_ids])
+    end
+
     if params[:status].present? && Registration::STATUS.include?(params[:status])
       @registrations = @registrations.by_status(params[:status])
     end
