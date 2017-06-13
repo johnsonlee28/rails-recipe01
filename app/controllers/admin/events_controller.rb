e22d1f5a7674934f4ba32d38c00599525598322e
Johnsonlee <johnsonlee28@gmail.com>
2017-06-13 13:28:09 +0800

    add bar chart js

--- app/controllers/admin/events_controller.rb
+++ app/controllers/admin/events_controller.rb
@@ -6,11 +6,47 @@ class Admin::EventsController < AdminController
 
   def show
     @event = Event.find_by_friendly_id!(params[:id])
+
+    colors = ['rgba(255, 99, 132, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(153, 102, 255, 0.2)',
+              'rgba(255, 159, 64, 0.2)'
+            ]
+
+    ticket_names = @event.tickets.map { |t| t.name }
+
+    status_colors = { "confirmed" => "#FF6384",
+                      "pending" => "#36A2EB"}
+
+    @data1 = {
+      labels: ticket_names,
+      datasets: Registration::STATUS.map do |s|
+        {
+          label:I18n.t(s, :scope => "registration.status"),
+          data: @event.tickets.map{ |t| t.registrations.by_status(s).count },
+          backgroundColor: status_colors[s],
+          borderWidth: 1
+        }
+      end
+    }
+
+    @data2 = {
+      labels: ticket_names,
+      datasets: [{
+        label: '# of Amount',
+        data: @event.tickets.map{ |t| t.registrations.by_status("confirmed").count * t.price},
+        backgroundColor: colors,
+        borderWidth: 1
+      }]
+    }
   end
 
   def new
     @event = Event.new
     @event.tickets.build
+    @event.attachments.build
   end
 
   def create
@@ -27,6 +63,7 @@ class Admin::EventsController < AdminController
     @event = Event.find_by_friendly_id!(params[:id])
     @event.tickets.build
     @event.tickets.build if @event.tickets.empty?
+    @event.attachments.build if @event.attachments.empty?
   end
 
   def update
@@ -80,7 +117,7 @@ class Admin::EventsController < AdminController
   protected
 
   def event_params
-    params.require(:event).permit(:name, :logo, :remove_logo, :remove_images,  :description, :friendly_id, :status, :category_id, :images => [],  :tickets_attributes => [:id, :name, :description, :price, :_destroy])
+    params.require(:event).permit(:name, :logo, :remove_logo, :remove_images,  :description, :friendly_id, :status, :category_id, :images => [],  :tickets_attributes => [:id, :name, :description, :price, :_destroy], :attachments_attributes => [:id, :attachment, :description, :_destroy])
   end
 
 end
